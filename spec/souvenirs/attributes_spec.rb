e7068235477eed5396d4d7a1f634511d32d9a672
mathieul <mathieul@gmail.com>
2011-01-31 23:06:40 -0800

    started writting attributes instance specs

--- spec/souvenirs/attributes_spec.rb
+++ spec/souvenirs/attributes_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Souvenirs::Attributes do
-  uses_constants("Boat")
+  uses_constants("Boat", "User")
 
   describe "the class" do
     it "can declare attributes using #attribute" do
@@ -14,6 +14,21 @@ describe Souvenirs::Attributes do
       Boat.attribute :color, :default => "black", :read_only => true
       Boat.attribute[:color].default_value.should == "black"
       Boat.attribute[:color].read_only.should be_true
+    end
+  end
+
+  describe "an instance" do
+    before(:all) do
+      User.attribute :name
+      User.attribute :age, :default => "18"
+      User.attribute :ssn, :read_only => true
+    end
+
+    it "can be initialized with a hash of attribute values" do
+      user = User.new(:name => "jean", :age => "20", :ssn => "1234567890")
+      user.name.should == "jean"
+      user.age.should == "20"
+      user.ssn.should == "1234567890"
     end
   end
 end

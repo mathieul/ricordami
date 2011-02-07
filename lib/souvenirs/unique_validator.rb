79d942a26e2ecac5e9e00d0bb553b986bbfa7379
mathieul <mathieul@gmail.com>
2011-02-07 08:07:34 -0800

    finally finished #validates_uniqueness_of :)

--- lib/souvenirs/unique_validator.rb
+++ lib/souvenirs/unique_validator.rb
@@ -1,12 +1,13 @@
 module Souvenirs
   class UniqueValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
+      return true unless record.new_record? || record.send(:attribute_changed?, attribute)
       index_name = "all_#{attribute}s".to_sym
       index = record.class.indices[index_name]
       if index.include?(value)
         attr_def = record.class.attributes[attribute]
         unless record.persisted? && attr_def.read_only?
-          record.errors.add(attribute, "is already used")
+          record.errors.add(attribute, options[:message] || "is already used")
         end
       end
     end
--- spec/souvenirs/unique_validator_spec.rb
+++ spec/souvenirs/unique_validator_spec.rb
@@ -1,32 +1,39 @@
 require "spec_helper"
 
 describe Souvenirs::UniqueValidator do
-  subject { Souvenirs::UniqueValidator }
-
   uses_constants("Call")
 
   before(:each) do
     Call.attribute :name
-    @record = Call.new(:name => "sophie")
-    @validator = subject.new(:attributes => [:name])
   end
+  let(:record) { Call.new(:name => "sophie") }
+  let(:validator) { Souvenirs::UniqueValidator.new(:attributes => [:name]) }
 
   it "is an active model EachValidator" do
-    @validator.is_a?(ActiveModel::EachValidator)
+    validator.is_a?(ActiveModel::EachValidator)
   end
 
   it "#setup adds a unique index" do
-    @validator.setup(Call)
+    validator.setup(Call)
     Call.indices[:all_names].should be_a(Souvenirs::UniqueIndex)
   end
 
   it "#validate_each adds an error if the value is already used" do
-    @validator.setup(Call)
-    @record.save!
+    validator.setup(Call)
+    record.save!
 
     sophie = Call.new(:name => "sophie")
-    @validator.validate_each(sophie, :name, sophie.name)
+    validator.validate_each(sophie, :name, record.name)
     sophie.should have(1).error
     sophie.errors[:name].should == ["is already used"]
+  end
+
+  it "accepts an option :message to change the error message" do
+    validator = Souvenirs::UniqueValidator.new(:attributes => [:name], :message => "come on, man!")
+    validator.setup(Call)
+    record.save!
+    sophie = Call.new(:name => "sophie")
+    validator.validate_each(sophie, :name, record.name)
+    sophie.errors[:name].should == ["come on, man!"]
   end
 end

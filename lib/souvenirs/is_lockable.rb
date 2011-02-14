15b47d250d9a3b2af10517fdac5abf49a1dd5cae
mathieul <mathieul@gmail.com>
2011-02-13 17:11:31 -0800

    copied ohm's lock, but not much success yet (deadlocks) - maybe problem in 1.9.2? in copy? - TODO: try redis-objects version first

--- lib/souvenirs/is_lockable.rb
+++ lib/souvenirs/is_lockable.rb
@@ -3,6 +3,47 @@ module Souvenirs
     extend ActiveSupport::Concern
 
     module ClassMethods
+      def lock
+        lock!
+        puts "#{Thread.current.object_id}: AFTER LOCK"
+        yield
+        puts "#{Thread.current.object_id}: AFTER BLOCK"
+        self
+      ensure
+        unlock!
+        puts "#{Thread.current.object_id}: AFTER UNLOCK"
+      end
+
+      private
+
+      # see SETNX command description for more info on the locking algorithm:
+      # http://redis.io/commands/setnx
+      def lock!
+        key = Factory.key_name(:model_lock, :model => self)
+        puts "#{Thread.current.object_id}: key = #{key.inspect}"
+        until Souvenirs.driver.setnx(key, Time.now.to_f + 0.5)
+          puts "#{Thread.current.object_id}: ONE"
+          next unless timestamp = Souvenirs.driver.get(key)
+          puts "#{Thread.current.object_id}: TWO"
+          sleep(0.1) and next unless lock_expired?(timestamp)
+          puts "#{Thread.current.object_id}: THREE"
+          break unless timestamp = Souvenirs.driver.getset(key, Time.now.to_f + 0.5)
+          puts "#{Thread.current.object_id}: FOUR"
+          break if lock_expired?(timestamp)
+          puts "#{Thread.current.object_id}: FIVE"
+        end
+        puts "#{Thread.current.object_id}: SETNX => #{Souvenirs.driver.get(key)}"
+        true
+      end
+
+      def unlock!
+        key = Factory.key_name(:model_lock, :model => self)
+        Souvenirs.driver.del(key)
+      end
+
+      def lock_expired?(timestamp)
+        timestamp.to_f < Time.now.to_f
+      end
     end
 
     module InstanceMethods
--- spec/souvenirs/is_lockable_spec.rb
+++ spec/souvenirs/is_lockable_spec.rb
@@ -1,5 +1,25 @@
 require "spec_helper"
 
 describe Souvenirs::IsLockable do
-  uses_constants("Client")
+  uses_constants("Client", "Server")
+
+  it "can get only one lock at a time" do
+    Thread::abort_on_exception = true
+    ts1 = ts2 = nil
+    t1 = Thread.new do
+      Client.lock do
+        sleep(0.01)
+        ts1 = Time.now
+      end
+    end
+    t2 = Thread.new do
+      sleep(0.01)
+      Client.lock do
+        ts2 = Time.now
+      end
+    end
+    t1.join
+    t2.join
+    ts2.should > ts1
+  end
 end

80db2d7391de9e532bff0e43550fea1db1dd92fb
mathieul <mathieul@gmail.com>
2011-02-19 11:22:26 -0800

    started specing the pub/sub observer feature

--- spec/acceptance/stand_alone_model_spec.rb
+++ spec/acceptance/stand_alone_model_spec.rb
@@ -85,11 +85,11 @@ feature "Stand-alone model" do
 
   scenario "finding models using basic queries" do
     Singer.create(:username => "ben", :email => "benjamin@biolay.com",
-                   :first_name => "Benjamin", :last_name => "Biolay", :deceased => "false")
+                  :first_name => "Benjamin", :last_name => "Biolay", :deceased => "false")
     Singer.create(:username => "lucien", :email => "serge@gainsbourg.com",
-                   :first_name => "Serge", :last_name => "Gainsbourg", :deceased => "true")
+                  :first_name => "Serge", :last_name => "Gainsbourg", :deceased => "true")
     Singer.create(:username => "bashung", :email => "alain@bashung.com",
-                   :first_name => "Alain", :last_name => "Bashung", :deceased => "true")
+                  :first_name => "Alain", :last_name => "Bashung", :deceased => "true")
 
     deceased = Singer.and(:deceased => "true").all
     deceased.map(&:username).should =~ %w(lucien bashung)
@@ -151,5 +151,37 @@ feature "Stand-alone model" do
     page1.map(&:last_name).should == ["Aznavour", "Bashung", "Benabar", "Biolay", "Brasens"]
     page3 = Singer.sort(:last_name, :asc_alpha).paginate(:page => 3, :per_page => 5)
     page3.map(&:last_name).should == ["Montand", "Souchon"]
+  end
+
+  scenario "observers" do
+    messages = []
+    observer = Singer.observe do |event, info|
+      messages << [event, info]
+    end
+
+    serge = Singer.create(:username => "lucien", :email => "serge@gainsbourg.com",
+                           :first_name => "Lucient", :last_name => "Gainsbourg")
+    serge.first_name = "Serge"
+    serge.save
+    ben = Singer.create(:username => "ben", :email => "benjamin@biolay.com",
+                        :first_name => "Benjamin", :last_name => "Biolay", :deceased => "false")
+    alain = Singer.create(:username => "bashung", :email => "alain@bashung.com",
+                          :first_name => "Alain", :last_name => "Bashung", :deceased => "true")
+    alain.delete
+    ben.delete
+    serge.delete
+
+    Singer.stop_observer(observer)
+    observer.join
+    messages.should == [
+      [:created, {:id => "1"}],
+      [:updated, {:id => "1", :first_name => ["Lucien", "Serge"]}],
+      [:created, {:id => "2"}],
+      [:created, {:id => "3"}],
+      [:updated, {:id => "1", :last_name => ["Gainsbourg", "Gainsbare"], :deceased => ["false", "true"]}],
+      [:deleted, {:id => "3"}],
+      [:deleted, {:id => "2"}],
+      [:deleted, {:id => "1"}]
+    ]
   end
 end

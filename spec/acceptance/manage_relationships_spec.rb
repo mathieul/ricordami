6b9f12b85a3bac4c0e79caf7866d08cf9ec61cb6
Mathieu Lajugie <mlajugie@liveops.com>
2011-02-24 18:50:08 -0800

    written first relationships acceptance test

--- spec/acceptance/manage_relationships_spec.rb
+++ spec/acceptance/manage_relationships_spec.rb
@@ -7,10 +7,32 @@ class Singer
 
   attribute :first_name
   attribute :last_name
+
+  references_many :albums, :dependent => :delete
+end
+
+class Album
+  include Souvenirs::Model
+  include Souvenirs::CanHaveRelationships
+
+  attribute :title
+
+  referenced_in :singer
 end
 
 feature "Manage relationships" do
   scenario "has many / belongs to" do
-    pending
+    serge = Singer.create(:first_name => "Serge", :last_name => "Gainsbourg")
+    serge.albums.create(:title => "Melody Nelson").should be_true
+
+    marseillaise = serge.albums.build(:title => "Aux Armes etc...")
+    marseillaise.save.should be_true
+
+    singer = Singer.first
+    singer.albums.map(&:title).should =~ ["Melody Nelson", "Aux Armes etc..."]
+
+    Album.all.map(&:title).should =~ ["Melody Nelson", "Aux Armes etc..."]
+    singer.delete
+    Album.all.should be_empty
   end
 end

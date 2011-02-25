3d0b7dd1f88f80a5f25d1baa2a48ee30ca4b0eca
mathieul <mathieul@gmail.com>
2011-02-24 23:15:54 -0800

    updated relationships acceptance scenario

--- spec/acceptance/manage_relationships_spec.rb
+++ spec/acceptance/manage_relationships_spec.rb
@@ -8,7 +8,7 @@ class Singer
   attribute :first_name
   attribute :last_name
 
-  #references_many :albums, :dependent => :delete
+  references_many :albums, :dependent => :delete
 end
 
 class Album
@@ -16,22 +16,25 @@ class Album
   include Souvenirs::CanHaveRelationships
 
   attribute :title
+  attribute :year, :indexed => :value
 
-  #referenced_in :singer
+  referenced_in :singer
 end
 
 feature "Manage relationships" do
   scenario "has many / belongs to" do
     serge = Singer.create(:first_name => "Serge", :last_name => "Gainsbourg")
-    serge.albums.create(:title => "Melody Nelson").should be_true
+    serge.albums.create(:title => "Melody Nelson", :year => "1971").should be_true
 
-    marseillaise = serge.albums.build(:title => "Aux Armes etc...")
+    marseillaise = serge.albums.build(:title => "Aux Armes et cætera", :year => "1979")
     marseillaise.save.should be_true
 
     singer = Singer.first
-    singer.albums.map(&:title).should =~ ["Melody Nelson", "Aux Armes etc..."]
+    singer.albums.map(&:title).should =~ ["Melody Nelson", "Aux Armes et cætera"]
+    singer.albums.where(:year => "1979").map(&:title).should =~ ["Aux Armes et cætera"]
+    singer.albums.sort(:year, :desc_num).map(&:title).should == ["Aux Armes et cætera", "Melody Nelson"]
 
-    Album.all.map(&:title).should =~ ["Melody Nelson", "Aux Armes etc..."]
+    Album.all.map(&:title).should =~ ["Melody Nelson", "Aux Armes et cætera"]
     singer.delete
     Album.all.should be_empty
   end
